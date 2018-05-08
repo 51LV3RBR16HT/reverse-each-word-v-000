293f428454d6938729968aae25d44870baed3b72
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 21:03:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
 def reverse_each_word(sentence1)
   sentence1.split.collect(&:reverse!).join(" ") do |sentence|
+# Or can be written as
+# sentence.split.collect {|word| word.reverse}.join(" ")
   end
 end
